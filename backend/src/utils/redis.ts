0311b62243fedab34b3467da436657678568ee9c
Michał Maj <maju@majudev.net>
2025-04-26 10:55:24 +0200

    Add redis reconnection mechanism

--- backend/src/utils/redis.ts
+++ backend/src/utils/redis.ts
@@ -8,13 +8,23 @@ redisClient.on("error", (err) => {
   console.error("Redis Client Error: ", err);
 });
 
-(async () => {
+redisClient.on("end", () => {
+  console.warn("Redis client disconnected. Attempting to reconnect...");
+  tryConnect();
+});
+
+const tryConnect = (async () => {
   try {
     await redisClient.connect();
     console.info("Redis client connected");
   } catch (error) {
-    console.error("Failed to connect to Redis:", error);
+    console.error("Failed to connect to Redis: ", error, "\nRetrying in 5s.");
+    setTimeout(() => {
+      tryConnect();
+    }, 5000);
   }
-})();
+});
+
+tryConnect();
 
 export default redisClient;
